edc37b87ca0582a8bdd361d301dd345a792ce8f9
yash.rjt <esharaagav@outlook.com>
2019-06-04 13:02:59 -0400

    Initial commit!

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewChild, ElementRef, OnChanges, AfterViewInit, DoCheck } from '@angular/core';
+
+interface Course{
+  name:string,
+  fee:number
+}
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit,DoCheck{
+ 
+  
+  
+
+  header:string;
+  x:number=100;
+  y:number=200;
+  available:boolean=true;
+  name:string="Eric";
+  company:string='RJT';
+  courses:Course[]=[
+    {
+      name:'java',
+      fee:1000
+    },
+    {
+      name:'android',
+      fee:2000
+    },
+    {
+      name:'Ios',
+      fee:3000
+    }
+  ];
+ 
+  @ViewChild('ssn')ssn:ElementRef;
+ 
+  @ViewChild('template')templateVariable:ElementRef;
+
+  constructor() { 
+    this.header="Trainers";
+    console.log(this.ssn);
+    
+  }
+
+  ngOnInit() {
+    console.log(this.templateVariable)
+  }
+
+  ngDoCheck(): void {   
+    console.log(this.ssn)
+  }
+  
+
+  handleClick($event){
+  console.log("TCL: StudentComponent -> handleClick -> event", event)
+    
+    console.log('button clicked')
+  }
+  handleSSN(value){
+  console.log("TCL: StudentComponent -> handleSSN -> value", value)
+  }
+}
